8c1fdde27c73d5052746daccd0c92465f5d8255a
TRANZUMPK\wajahat.ejaz <wajahat.ejaz@tcs.com.pk>
2022-02-19 02:30:31 +0500

    worked on cart Item

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from unicodedata import category
 from django.shortcuts import get_object_or_404, render
+from carts.models import CartItem
+from carts.views import _cart_id
 from categories.models import Categories
 
 from store.models import Product
@@ -13,7 +15,7 @@ def storeView(request,category_slug=None):
     
     if category_slug != None:
         categories = get_object_or_404(Categories,slug=category_slug)
-        print("cat",categories)
+        
         products   = Product.objects.filter(category=categories,is_available=True)
         counts      = products.count()
     else:
@@ -31,10 +33,13 @@ def storeView(request,category_slug=None):
 def productView(request,category_slug,product_slug):
 
     try:
-        single_product= Product.objects.get(category__slug=category_slug,slug=product_slug)
+        single_product  = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart         = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
+
     except Exception as e:
         raise e
     context={
-        "single_product":single_product
+        "single_product":single_product,
+        "in_cart"         :in_cart
     }
     return render(request,"store/product.html",context)
